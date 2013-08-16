b8c291639b0ac0fa7717c7ca8a00b56ca80401ea
EPMSOGonza <joke_a_87@hotmail.com>
2013-08-16 14:42:08 +0200

    Basic login form and controlle funktion

--- public/javascripts/app/controllers.js
+++ public/javascripts/app/controllers.js
@@ -5,8 +5,18 @@
 function IndexController($scope) {
 }
 
-function LandingController($scope) {
+function LandingController($scope, $http) {
   $scope.message = "There is no spoon. Revenge!";
+  $scope.submitLogin = function() {
+    var data = {'username': $scope.username,
+                'password': $scope.password};
+    $http.post("login", data)
+      .success(function(data, status) {
+        // $scope.email = "";
+        // $scope.message = "";
+      });
+      // $scope.messageSent = true;
+  };
 }
 
 function TestController($scope) {
