532c2984e0971265dfa3f8dd9ceb6289178fa253
robin <robin.edman@gmail.com>
2013-12-02 23:04:12 +0100

    More tests and cleaned things up even more

--- tests/expense_and_debt_test.rb
+++ tests/expense_and_debt_test.rb
@@ -31,24 +31,54 @@ module ExpenseAndDebtTest
     end
   end
 
-  class SplitEqually < Base
-    def test_only_puts_one_of_us_in_debt
-      expense = expense_splitted_equally
+  class BuySomeoneSomething < Base
+    def setup
+      super
+      @expense = buy_someone_something
+    end
 
-      assert_equal(1, expense.debts.length)
+
+    def test_only_puts_one_of_us_in_debt
+      assert_equal(1, @expense.debts.length)
     end
 
     def test_puts_someone_in_debt_to_me
-      expense = expense_splitted_equally
+      assert_equal(@first_debtor.email, @expense.debts.first.debtor)
+      assert_equal(@creditor.email, @expense.debts.first.creditor)
+    end
 
-      assert_equal('first_debtor@example.com', expense.debts.first.debtor)
-      assert_equal('creditor@example.com', expense.debts.first.creditor)
+    def test_makes_someone_owe_that_amount
+      assert_equal(105, @expense.debts.first.amount)
+    end
+
+    private
+
+    def buy_someone_something
+      expense = @creditor.expenses.create!(amount: 105)
+      expense.split([{user: @first_debtor.email, amount: 105}])
+      return expense
+    end
+
+  end
+
+  class SplitEqually < Base
+    def setup
+      super
+      @expense = expense_splitted_equally
+    end
+
+
+    def test_only_puts_one_of_us_in_debt
+      assert_equal(1, @expense.debts.length)
+    end
+
+    def test_puts_someone_in_debt_to_me
+      assert_equal(@first_debtor.email, @expense.debts.first.debtor)
+      assert_equal(@creditor.email, @expense.debts.first.creditor)
     end
 
     def test_makes_someone_owe_half_the_amount
-      expense = expense_splitted_equally
-      
-      assert_equal((expense.amount / 2), expense.debts.first.amount)
+      assert_equal((@expense.amount / 2), @expense.debts.first.amount)
     end
 
     private
@@ -61,23 +91,23 @@ module ExpenseAndDebtTest
   end
 
   class SplitInequally < Base
-    def test_only_puts_one_of_us_in_debt
-      expense = expense_splitted_inequally
+    def setup
+      super
+      @expense = expense_splitted_inequally
+    end
 
-      assert_equal(1, expense.debts.length)
+
+    def test_only_puts_one_of_us_in_debt
+      assert_equal(1, @expense.debts.length)
     end
 
     def test_puts_someone_in_debt_to_me
-      expense = expense_splitted_inequally
-
-      assert_equal('first_debtor@example.com', expense.debts.first.debtor)
-      assert_equal('creditor@example.com', expense.debts.first.creditor)
+      assert_equal('first_debtor@example.com', @expense.debts.first.debtor)
+      assert_equal('creditor@example.com', @expense.debts.first.creditor)
     end
 
     def test_makes_someone_owe_me_that_amount
-      expense = expense_splitted_inequally
-
-      assert_equal(700, expense.debts.first.amount)
+      assert_equal(700, @expense.debts.first.amount)
     end
 
     def test_raises_exception_when_split_does_not_add_up
